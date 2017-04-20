3b6efd0e834c1ea25893136efcb8857825ae5825
John Beadle <jbeadle@liveperson.com>
2017-04-20 20:38:50 +0100

    WIP sampleapp04 with SDK 2.1.5 builds OK

---
+++ apps/sampleapp04/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "com.liveperson.messagingSDK.lpMessagingSDK",
+        "file": "plugins/com.liveperson.messagingSDK/www/lpMessagingSDK.js",
+        "pluginId": "com.liveperson.messagingSDK",
+        "clobbers": [
+            "lpMessagingSDK"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.2",
+    "com.liveperson.messagingSDK": "0.1.0"
+};
+// BOTTOM OF METADATA
+});
