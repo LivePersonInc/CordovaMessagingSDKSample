b111ac10f167421fcf08e59ad8611493d5eb0489
John Beadle <jbeadle@liveperson.com>
2017-05-10 19:00:59 +0100

    Android v2.1.5 update sampleApp03 updated and tested with Android SDK v2.1.5 libs FIXED: plugin/v1.5 .java file updated with correct code - was containing an older outdated version

--- apps/sampleapp03/platforms/android/lp_messaging_sdk/build/generated/source/buildConfig/release/com/liveperson/messaging/sdk/BuildConfig.java
+++ apps/sampleapp03/platforms/android/lp_messaging_sdk/build/generated/source/buildConfig/release/com/liveperson/messaging/sdk/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.liveperson.messaging.sdk";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 213;
-  public static final String VERSION_NAME = "2.1.3";
+  public static final int VERSION_CODE = 215;
+  public static final String VERSION_NAME = "2.1.5";
 }
