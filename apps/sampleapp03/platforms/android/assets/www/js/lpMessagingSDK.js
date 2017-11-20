bfad1f4b8320c6ea9622da79c9e1626591918449
John Beadle <jbeadle@liveperson.com>
2017-11-20 23:35:22 +0000

    WIP demo app ported from Android... errors with missing plugin calls ?! unsure of the cause...

--- apps/sampleapp03/platforms/android/assets/www/js/lpMessagingSDK.js
+++ apps/sampleapp03/platforms/android/assets/www/js/lpMessagingSDK.js
@@ -4,14 +4,14 @@ refer to README for full explanation and documentation
 var exec = require('cordova/exec');
 
 exports.lp_conversation_api = function(action, args, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "LPMessagingSDK", action, args);
+  exec(successCallback, errorCallback, 'LPMessagingSDK', action, args);
 };
 
 exports.lp_register_event_callback = function(args, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "LPMessagingSDK", "lp_register_event_callback", args);
+  exec(successCallback, errorCallback, 'LPMessagingSDK', 'lp_register_event_callback', args);
 };
 
 exports.hello_world = function(action, args, successCallback, errorCallback) {
-    console.log("999 HELLO WORLD");
-    successCallback("exports.hello_world");
+  console.log('999 HELLO WORLD');
+  successCallback('exports.hello_world');
 };
--- apps/sampleapp03/platforms/android/assets/www/plugins/com.liveperson.messagingSDK/www/lpMessagingSDK.js
+++ apps/sampleapp03/platforms/android/assets/www/plugins/com.liveperson.messagingSDK/www/lpMessagingSDK.js
@@ -1,19 +1,19 @@
-cordova.define("com.liveperson.messagingSDK.lpMessagingSDK", function(require, exports, module) {
+cordova.define('com.liveperson.messagingSDK.lpMessagingSDK', function(require, exports, module) {
 /*global cordova, module
 refer to README for full explanation and documentation
 */
-var exec = require('cordova/exec');
+  var exec = require('cordova/exec');
 
-exports.lp_conversation_api = function(action, args, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "LPMessagingSDK", action, args);
-};
+  exports.lp_conversation_api = function(action, args, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'LPMessagingSDK', action, args);
+  };
 
-exports.lp_register_event_callback = function(args, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "LPMessagingSDK", "lp_register_event_callback", args);
-};
+  exports.lp_register_event_callback = function(args, successCallback, errorCallback) {
+    exec(successCallback, errorCallback, 'LPMessagingSDK', 'lp_register_event_callback', args);
+  };
 
-exports.hello_world = function(action, args, successCallback, errorCallback) {
-    console.log("999 HELLO WORLD");
-    successCallback("exports.hello_world");
-};
+  exports.hello_world = function(action, args, successCallback, errorCallback) {
+    console.log('999 HELLO WORLD');
+    successCallback('exports.hello_world');
+  };
 });
