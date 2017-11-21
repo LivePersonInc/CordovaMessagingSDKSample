737c7f63fd808e06e88efa5749582fe3c4817584
John Beadle <jbeadle@liveperson.com>
2017-11-21 00:13:16 +0000

    WIP 2.8 methods not found in plugin

--- apps/sampleapp01/platforms/ios/platform_www/cordova_plugins.js
+++ apps/sampleapp01/platforms/ios/platform_www/cordova_plugins.js
@@ -1,28 +1,28 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
-  {
-    "id": "phonegap-plugin-push.PushNotification",
-    "file": "plugins/phonegap-plugin-push/www/push.js",
-    "pluginId": "phonegap-plugin-push",
-    "clobbers": [
-      "PushNotification"
-    ]
-  },
-  {
-    "id": "com.liveperson.messagingSDK.lpMessagingSDK",
-    "file": "plugins/com.liveperson.messagingSDK/www/lpMessagingSDK.js",
-    "pluginId": "com.liveperson.messagingSDK",
-    "clobbers": [
-      "lpMessagingSDK"
-    ]
-  }
+    {
+        "id": "phonegap-plugin-push.PushNotification",
+        "file": "plugins/phonegap-plugin-push/www/push.js",
+        "pluginId": "phonegap-plugin-push",
+        "clobbers": [
+            "PushNotification"
+        ]
+    },
+    {
+        "id": "com.liveperson.messagingSDK.lpMessagingSDK",
+        "file": "plugins/com.liveperson.messagingSDK/www/lpMessagingSDK.js",
+        "pluginId": "com.liveperson.messagingSDK",
+        "clobbers": [
+            "lpMessagingSDK"
+        ]
+    }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-whitelist": "1.3.3",
-  "phonegap-plugin-push": "2.0.0",
-  "com.liveperson.messagingSDK": "0.1.0"
+    "cordova-plugin-whitelist": "1.3.3",
+    "phonegap-plugin-push": "2.0.0",
+    "com.liveperson.messagingSDK": "0.1.0"
 };
 // BOTTOM OF METADATA
 });
